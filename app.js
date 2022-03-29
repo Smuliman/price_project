57f4bb25add126ef8fb58ad5ab6b08f0776f2c61
Smulimaan <samuli.tyyska@live.com>
2022-03-29 20:29:20 +0300

    commit 29.3. first

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.locals.pretty = true;
+
+//luodaan reitit
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+app.get("/hello", function (req, res) {
+  const m3o = require("@m3o/m3o-node");
+
+  new m3o.Client({ token: "MTk3ZTNkZmEtNjExOS00MjBkLThkNDktNWI2OGE0YTVkYTZh" })
+    .call("helloworld", "call", { name: "Samuli" })
+    .then((response) => {
+      console.log(response);
+      res.send("Hello World!" + JSON.stringify(response));
+    });
+});
+app.get("/commodites", function (req, res) {
+  const m3o = require("@m3o/m3o-node");
+  var hinta = "";
+
+  new m3o.Client({ token: "MTk3ZTNkZmEtNjExOS00MjBkLThkNDktNWI2OGE0YTVkYTZh" })
+    .call("price", "get", { name: "bitcoin", currency: "USD" })
+    .then((response) => {
+      hinta = response;
+      console.log(response);
+      //res.send(JSON.stringify(response));
+    });
+  app.render("pages/commodites", hinta);
+});
+app.get("/oil", function (req, res) {
+  res.send("Oil prices are high!");
+});
+app.get("*", function (req, res) {
+  res.send("Can't find the requested page!", 404);
+});
+//Luodaan web-palvelin
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
