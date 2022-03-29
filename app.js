1096e36c641f1bc03b210160f73e95dd142f4859
Smulimaan <samuli.tyyska@live.com>
2022-03-29 22:53:13 +0300

    commit 29.3. third

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
 const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
-app.locals.pretty = true;
+//app.locals.pretty = true;
 
 //luodaan reitit
 app.get("/", function (req, res) {
